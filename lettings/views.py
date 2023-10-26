0bdb55c943116d9d48a861f8ce9d543907c17886
Tortique <tortiquedetaharia@gmail.com>
2023-10-26 11:59:34 +0200

    Migrations des vues et urls

--- lettings/views.py
+++ lettings/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from lettings.models import Letting
+
+
+def index(request):
+    lettings_list = Letting.objects.all()
+    context = {'lettings_list': lettings_list}
+    return render(request, 'lettings/index.html', context)
+
+
+def letting(request, letting_id):
+    letting_detail = Letting.objects.get(id=letting_id)
+    context = {
+        'title': letting_detail.title,
+        'address': letting_detail.address,
+    }
+    return render(request, 'lettings/letting.html', context)
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from profiles.models import Profile
+
+
+def index(request):
+    profiles_list = Profile.objects.all()
+    context = {'profiles_list': profiles_list}
+    return render(request, 'profiles/index.html', context)
+
+
+def profile(request, username):
+    profile_detail = Profile.objects.get(user__username=username)
+    context = {'profile': profile_detail}
+    return render(request, 'profiles/profile.html', context)
